2da5bcfb4a0bcfc7ad9a16f4a21868e59eec2a4e
aniket079 <124426864+aniket079@users.noreply.github.com>
2026-02-07 23:59:37 +0530

    c1

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+// import userRouter from "./routes/users.routes.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+const app = express();
+
+
+dotenv.config();
+connectDB();
+
+
+app.use(express.json());
+
+// BASE ROUTE
+app.get("/", (req, res) => {
+  res.send("User Management API is running");
+});
+
+// USER ROUTES
+
+export default app;
